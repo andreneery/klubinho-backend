b19e11dddd2eb390c4c502533a62221e7ecb920d
andreneery <neryandre8@gmail.com>
2023-11-13 23:50:12 -0300

    ajustando reuniao

--- backend/klubinho-backend/database/migrations/2023_11_11_225128_create_reuniao_table.php
+++ backend/klubinho-backend/database/migrations/2023_11_11_225128_create_reuniao_table.php
@@ -9,7 +9,7 @@ return new class extends Migration
 
     public function up(): void
     {
-        Schema::create('reuniao', function (Blueprint $table) {
+        Schema::create('reuniaos', function (Blueprint $table) {
             $table->id();
             $table->string('titulo')->notNullable();
             $table->string('descricao')->nullable();
