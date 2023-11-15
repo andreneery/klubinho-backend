3f8e88e2552f763b4bd55be51d3ff6a70d50ec3a
andreneery <neryandre8@gmail.com>
2023-11-15 15:50:59 -0300

    colocando o post por ordem decrescente

--- backend/klubinho-backend/app/Http/Controllers/PostController.php
+++ backend/klubinho-backend/app/Http/Controllers/PostController.php
@@ -59,6 +59,7 @@ class PostController extends Controller
             $post = Post::where('club_id', $id)
             ->join('users', 'posts.user_id', '=', 'users.id')
             ->select('posts.*', 'users.name', 'users.last_name', 'users.profile_picture')
+            ->orderBy('posts.created_at', 'desc')
             ->get();
             return response($post, 200);
         } else {
